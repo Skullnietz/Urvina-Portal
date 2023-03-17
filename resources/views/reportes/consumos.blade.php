f9b161f40eea5b6aaa0948ac20a561349e250b32
Carlos Enrique Guizar Peña <71019682+Skullnietz@users.noreply.github.com>
2023-03-17 17:05:25 -0600

    Funcionalidad Grafico Consumo

--- resources/views/reportes/consumos.blade.php
+++ resources/views/reportes/consumos.blade.php
@@ -402,7 +402,7 @@ Highcharts.chart('container', {
   },
 
   xAxis: {
-    categories: [@foreach ($dataConsulta as $dataSD) $dataSD->Descripcion, @endforeach],
+    categories: [@foreach ($dataConsulta as $dataSD) {{__('$dataSD->Descripcion'),}} @endforeach],
   },
   yAxis: {
     title: {
