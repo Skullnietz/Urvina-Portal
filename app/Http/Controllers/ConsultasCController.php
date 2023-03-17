f6ef6e63afce42975b21b0e5bc473017eab980a0
Carlos Enrique Guizar Peña <71019682+Skullnietz@users.noreply.github.com>
2023-03-17 13:44:59 -0600

    Integracion de departamentos, graficas

--- app/Http/Controllers/ConsultasCController.php
+++ app/Http/Controllers/ConsultasCController.php
@@ -8,9 +8,15 @@ class ConsultasCController extends Controller
 {
     public function index(){
         session_start();
+        $departamentos = DB::select(
+            "EXEC spDepartamentosApp :id",
+            [
+                "id" => $_SESSION['usuario']->UsuarioCteCorp,
+            ]
+        );
         ////////////// Vista Consultas del Cliente /////////////
         if(isset($_SESSION['usuario'])){
-            return view('consultasc');
+            return view('consultasc')->with('departamentos',$departamentos);
         }else {
             return redirect()->route('login', app()->getLocale());
         }
