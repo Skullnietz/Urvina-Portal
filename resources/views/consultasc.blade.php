39ee1cd0623f8bb285e4d48f0df1a31b857a7938
Carlos Enrique Guizar Peña <71019682+Skullnietz@users.noreply.github.com>
2023-03-21 12:25:07 -0600

    Ajuste en formulario grafica

--- resources/views/consultasc.blade.php
+++ resources/views/consultasc.blade.php
@@ -151,7 +151,28 @@
                         <div class="card">
                         <div class="card-header bg-primary" data-card-widget="collapse"> <center><b>{{__('Información detallada (Opcional)')}}</b></center></div>
                         <div class="card-body">
+
                         <form action="{{route('reporteC', app()->getLocale())}}" method="get">
+                            <ul class="nav nav-tabs">
+                                <li class="active"><a data-toggle="tab" href="#home">Home</a></li>
+                                <li><a data-toggle="tab" href="#menu1">Menu 1</a></li>
+                                <li><a data-toggle="tab" href="#menu2">Menu 2</a></li>
+                              </ul>
+
+                              <div class="tab-content">
+                                <div id="home" class="tab-pane fade in active">
+                                  <h3>HOME</h3>
+                                  <p>Some content.</p>
+                                </div>
+                                <div id="menu1" class="tab-pane fade">
+                                  <h3>Menu 1</h3>
+                                  <p>Some content in menu 1.</p>
+                                </div>
+                                <div id="menu2" class="tab-pane fade">
+                                  <h3>Menu 2</h3>
+                                  <p>Some content in menu 2.</p>
+                                </div>
+                              </div>
 
                         <div class="row">
                             <div class="col"><b>{{__('Desde')}}</b> </div>
