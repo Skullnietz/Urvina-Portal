65e63469f1d8a72e9f714c4d44cf1299d1e1ba3e
Carlos Enrique Guizar Peña <71019682+Skullnietz@users.noreply.github.com>
2023-03-21 14:23:36 -0600

    Ajuste en formulario grafica

--- resources/views/reportes/consumos.blade.php
+++ resources/views/reportes/consumos.blade.php
@@ -107,168 +107,173 @@
         <!-- Formulario de Reportes-->
         <div class="row" >
             <div class="col">
-        <div class="card collapsed-card">
+                <div class="card collapsed-card">
 
-            <div class="card-header bg-success">
-                <h5 class="card-title">{{__('Reportes')}}</h5>
-                <div class="card-tools">
-                    <button type="button" class="btn btn-tool" data-card-widget="collapse">
-                    <i style="color:white" class="fas fa-minus"></i>
-                    </button>
-                    <button type="button" class="btn btn-tool" data-card-widget="remove">
-                    <i style="color:white" class="fas fa-times"></i>
-                    </button>
+                    <div class="card-header bg-success">
+                        <h5 class="card-title">{{__('Reportes')}}</h5>
+                        <div class="card-tools">
+                            <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                            <i style="color:white" class="fas fa-minus"></i>
+                            </button>
+                            <button type="button" class="btn btn-tool" data-card-widget="remove">
+                            <i style="color:white" class="fas fa-times"></i>
+                            </button>
+                            </div>
                     </div>
-            </div>
-            <div class="card-body">
-                <div class="row">
-                    <form action="{{route('reporteC', app()->getLocale())}}" method="get">
-                    <div class="col-3">
-                        <div class="card">
+                    <div class="card-body">
+                        <form action="{{route('reporteC', app()->getLocale())}}" method="get">
+                        <div class="row">
+                            <div class="col-3">
+                                <div class="card">
 
-                            <div class="card-header bg-secondary"><center><b>{{__('Tipo de Reporte')}}</b></center></div>
-                            <div class="card-body">
-                                <div class="row">
-                                    <div class="col">
-                                        <div class="input-group">
-                                            <div class="input-group-prepend">
-                                                <div class="input-group-text">
-                                                    <input type="radio" name="tipo" value="Consumo"
-                                                        aria-label="Radio button for following text input">
+                                    <div class="card-header bg-secondary"><center><b>{{__('Tipo de Reporte')}}</b></center></div>
+                                    <div class="card-body">
+                                        <div class="row">
+                                            <div class="col">
+                                                <div class="input-group">
+                                                    <div class="input-group-prepend">
+                                                        <div class="input-group-text">
+                                                            <input type="radio" name="tipo" value="Consumo"
+                                                                aria-label="Radio button for following text input">
+                                                        </div>
+                                                    </div>
+                                                    <b class="form-control">{{__('Consumo')}}</b>
                                                 </div>
                                             </div>
-                                            <b class="form-control">{{__('Consumo')}}</b>
-                                        </div>
-                                    </div>
 
-                                </div>
-                                <div class="row">
-                                    <div class="col">
-                                        <div class="input-group">
-                                            <div class="input-group-prepend">
-                                                <div class="input-group-text">
-                                                    <input name="tipo" type="radio" value="Departamento"
-                                                        aria-label="Radio button for following text input">
+                                        </div>
+                                        <div class="row">
+                                            <div class="col">
+                                                <div class="input-group">
+                                                    <div class="input-group-prepend">
+                                                        <div class="input-group-text">
+                                                            <input name="tipo" type="radio" value="Departamento"
+                                                                aria-label="Radio button for following text input">
+                                                        </div>
+                                                    </div>
+                                                    <b class="form-control">{{__('Departamento')}}</b>
                                                 </div>
                                             </div>
-                                            <b class="form-control">{{__('Departamento')}}</b>
-                                        </div>
-                                    </div>
 
-                                </div>
-                                <div class="row">
-                                    <div class="col">
-                                        <div class="input-group">
-                                            <div class="input-group-prepend">
-                                                <div class="input-group-text">
-                                                    <input type="radio" name="tipo" value="Equipo"
-                                                        aria-label="Radio button for following text input">
+                                        </div>
+                                        <div class="row">
+                                            <div class="col">
+                                                <div class="input-group">
+                                                    <div class="input-group-prepend">
+                                                        <div class="input-group-text">
+                                                            <input type="radio" name="tipo" value="Equipo"
+                                                                aria-label="Radio button for following text input">
+                                                        </div>
+                                                    </div>
+                                                    <b class="form-control">{{__('Equipo')}}</b>
                                                 </div>
                                             </div>
-                                            <b class="form-control">{{__('Equipo')}}</b>
-                                        </div>
-                                    </div>
 
-                                </div>
-                                <div class="row">
-                                    <div class="col">
-                                        <div class="input-group">
-                                            <div class="input-group-prepend">
-                                                <div class="input-group-text">
-                                                    <input type="radio" name="tipo" value="Anual"
-                                                        aria-label="Radio button for following text input">
+                                        </div>
+                                        <div class="row">
+                                            <div class="col">
+                                                <div class="input-group">
+                                                    <div class="input-group-prepend">
+                                                        <div class="input-group-text">
+                                                            <input type="radio" name="tipo" value="Anual"
+                                                                aria-label="Radio button for following text input">
+                                                        </div>
+                                                    </div>
+                                                    <b class="form-control">{{__('Anual')}}</b>
                                                 </div>
                                             </div>
-                                            <b class="form-control">{{__('Anual')}}</b>
                                         </div>
+
                                     </div>
                                 </div>
+
 
                             </div>
+                            <div class="col-9">
+                                <div class="card">
+                                <div class="card-header bg-primary" data-card-widget="collapse"> <center><b>{{__('Información detallada (Opcional)')}}</b></center></div>
+                                <div class="card-body">
+
+
+                                    <ul class="nav nav-tabs">
+                                        <li style="margin-right:10px"><a class="btn btn-secondary checked" data-toggle="tab" href="#fecha">{{__('Fecha')}}</a></li>
+                                        <li style="margin-right:10px"><a class="btn btn-secondary checked" data-toggle="tab" href="#articulo">{{__('Articulo')}}</a></li>
+                                        <li style="margin-right:10px"><a class="btn btn-secondary checked" data-toggle="tab" href="#equipo">{{__('Equipo')}}</a></li>
+                                        <li style="margin-right:10px"><a class="btn btn-secondary checked" data-toggle="tab" href="#departamento">{{__('Departamento')}}</a></li>
+                                    </ul>
+
+                                      <div class="tab-content" style="margin-top:10px">
+
+                                        <div id="fecha" class="tab-pane fade in active">
+                                            <div class="row">
+                                                <div class="col"><b>{{__('Desde')}}</b> </div>
+                                                <div class="col"><b>{{__('Hasta')}}</b> </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="col"><input name="desde" value="" class="form-control" type="date"></div>
+                                                <div class="col"><input name="hasta" value="" class="form-control" type="date"></div>
+
+                                            </div>
+                                        </div>
+                                        <div id="articulo" class="tab-pane fade">
+
+                                            <div class="row">
+                                                <div class="col"><b>{{__('Artículo')}}</b> </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="col"><input name="articulo" class="form-control" type="text" value=""></div>
+                                            </div><br>
+                                        </div>
+                                        <div id="equipo" class="tab-pane fade">
+
+                                            <div class="row">
+                                                <div class="col"><b>{{__('Equipo/Linea/Ref')}}</b></div>
+                                            </div>
+
+                                            <div class="row">
+
+                                                <div class="col"><input name="equipo" class="form-control" type="text" value=""></div>
+                                            </div><br>
+                                        </div>
+                                        <div id="departamento" class="tab-pane fade">
+
+                                            <div class="row">
+                                                <div class="col-3">
+                                                    <b>{{__('Departamento:')}}</b>
+
+                                                </div>
+                                                <div class="col-9">
+                                                    <select name="departamento" id="" class="form-control" required>
+                                                        @foreach ($departamentos as $departamento)
+                                                        <option value="0">{{__('Selecciona una opción')}}</option>
+                                                        <option value="{{$departamento->Departamento}}">{{__($departamento->Nombre)}}</option>
+                                                        @endforeach
+
+                                                    </select>
+                                                </div>
+                                            </div>
+                                          </div>
+                                      </div>
+
+                                </div>
+                            </div>
+                            <br>
                         </div>
 
+                            </div>
 
-                    </div>
-                    <div class="col-9">
-                        <div class="card">
-                        <div class="card-header bg-primary" data-card-widget="collapse"> <center><b>{{__('Información detallada (Opcional)')}}</b></center></div>
-                        <div class="card-body">
-
-
-                            <ul class="nav nav-tabs">
-                                <li style="margin-right:10px"><a class="btn btn-secondary checked" data-toggle="tab" href="#fecha">{{__('Fecha')}}</a></li>
-                                <li style="margin-right:10px"><a class="btn btn-secondary checked" data-toggle="tab" href="#articulo">{{__('Articulo')}}</a></li>
-                                <li style="margin-right:10px"><a class="btn btn-secondary checked" data-toggle="tab" href="#equipo">{{__('Equipo')}}</a></li>
-                                <li style="margin-right:10px"><a class="btn btn-secondary checked" data-toggle="tab" href="#departamento">{{__('Departamento')}}</a></li>
-                            </ul>
-
-                              <div class="tab-content" style="margin-top:10px">
-
-                                <div id="fecha" class="tab-pane fade in active">
-                                    <div class="row">
-                                        <div class="col"><b>{{__('Desde')}}</b> </div>
-                                        <div class="col"><b>{{__('Hasta')}}</b> </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col"><input name="desde" value="" class="form-control" type="date"></div>
-                                        <div class="col"><input name="hasta" value="" class="form-control" type="date"></div>
-
-                                    </div>
-                                </div>
-                                <div id="articulo" class="tab-pane fade">
-
-                                    <div class="row">
-                                        <div class="col"><b>{{__('Artículo')}}</b> </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col"><input name="articulo" class="form-control" type="text" value=""></div>
-                                    </div><br>
-                                </div>
-                                <div id="equipo" class="tab-pane fade">
-
-                                    <div class="row">
-                                        <div class="col"><b>{{__('Equipo/Linea/Ref')}}</b></div>
-                                    </div>
-
-                                    <div class="row">
-
-                                        <div class="col"><input name="equipo" class="form-control" type="text" value=""></div>
-                                    </div><br>
-                                </div>
-                                <div id="departamento" class="tab-pane fade">
-
-                                    <div class="row">
-                                        <div class="col-3">
-                                            <b>{{__('Departamento:')}}</b>
-
-                                        </div>
-                                        <div class="col-9">
-                                            <select name="departamento" id="" class="form-control" required>
-                                                @foreach ($departamentos as $departamento)
-                                                <option value="0">{{__('Selecciona una opción')}}</option>
-                                                <option value="{{$departamento->Departamento}}">{{__($departamento->Nombre)}}</option>
-                                                @endforeach
-
-                                            </select>
-                                        </div>
-                                    </div>
-                                  </div>
-                              </div>
 
                         </div>
+
                     </div>
-                    <br>
+                    <div class="card-footer">
+                        <center>
+                            <button type="submit" class="btn btn-primary">{{__('Consultar')}}</button>
+                        </center>
+                    </form>
+
+                    </div>
                 </div>
-
-            </div>
-            <div class="card-footer">
-                <center>
-                    <button type="submit" class="btn btn-primary">{{__('Consultar')}}</button>
-                </center>
-            </form>
-
-            </div>
-        </div>
     </div>
     </div>
         <!-- FIN Formulario de Reportes-->
