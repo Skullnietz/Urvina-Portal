5047f66d4784020c8c84cefea3538b586c3951b6
Carlos Enrique Guizar Peña <71019682+Skullnietz@users.noreply.github.com>
2023-03-31 13:01:42 -0600

    Formatear fechas

--- resources/views/item.blade.php
+++ resources/views/item.blade.php
@@ -254,12 +254,12 @@ $limite = $articulo[0]->Mensaje2;
 
                                         <tr>
                                             <td class="border"> <b>{{__('De:')}}</b> </td>
-                                            <td class="border" style="text-align:right">{{$primerDia}}</td>
+                                            <td class="border" style="text-align:right">{{new Date($primerDia)}}</td>
 
                                         </tr>
                                         <tr>
                                             <td class="border"> <b>{{__('A:')}}</b> </td>
-                                            <td class="border" style="text-align:right"><span id="ultimo-dia-es">{{$ultimoDia}}</td>
+                                            <td class="border" style="text-align:right"><span id="ultimo-dia-es">{{new Date($ultimoDia)}}</td>
                                         </tr>
                                     </table>
                                     <div class="row">
