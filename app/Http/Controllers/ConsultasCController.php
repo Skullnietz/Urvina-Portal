603093bcdcf0dea45e213247992651b855eaf95e
Carlos Enrique Guizar Peña <71019682+Skullnietz@users.noreply.github.com>
2023-04-10 15:12:13 -0600

    Parametros reporte excel

--- app/Http/Controllers/ConsultasCController.php
+++ app/Http/Controllers/ConsultasCController.php
@@ -185,7 +185,7 @@ class ConsultasCController extends Controller
     }
     }
 
-    public function ExcelReporteConsulta(){
+    public function ExcelReporteConsulta(Request $request,$pID,$pTipo,$pDepartamento,$pItem,$pReference,$pFrom,$pTo){
         session_start();
         $dataConsulta = DB::select(
             "EXEC spReportesApp :id,:type,:department,:item,:reference,:from,:to",
