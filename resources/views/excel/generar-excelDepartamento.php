f0157c98834ee0fe711bfaf0026bf516018b7e5d
Carlos Enrique Guizar Peña <71019682+Skullnietz@users.noreply.github.com>
2023-04-11 15:25:23 -0600

    Pruebas excel

--- resources/views/excel/generar-excelDepartamento.php
+++ resources/views/excel/generar-excelDepartamento.php
@@ -6,16 +6,14 @@ header("Content-Disposition: attachment; filename=datos-usuario.xls");
 <table border="1">
 
                             <tr class="bg-secondary">
-                                <th> <?php echo __('Articulo') ?> </th>
-                                <th><?php echo __('Descripcion')?></th>
+                                <th> <?php echo __('Departamento') ?> </th>
                                 <th><?php echo __('Cantidad')?></th>
                                 <th><?php echo __('Importe')?></th>
                             </tr>
 
                             <?php foreach ($dataConsulta as $dataD){ echo '
                                 <tr>
-                                <td>'.$dataD->Articulo.'</td>
-                                <td>'.__($dataD->Descripcion).'</td>
+                                <td>'.$dataD->Departamento.'</td>
                                 <td>'.number_format($dataD->Cantidad).'</td>
                                 <td>'.number_format($dataD->Importe, 2, '.', '').'</td>
                                 </tr> ';
