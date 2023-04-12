d60c022bf29ca479eb28c9513e331deec0e405f6
Carlos Enrique Guizar Peña <71019682+Skullnietz@users.noreply.github.com>
2023-04-12 08:43:23 -0600

    Creación de excel

--- resources/views/reportes/consumos.blade.php
+++ resources/views/reportes/consumos.blade.php
@@ -362,14 +362,14 @@ display: none !important;
                 <div class="card-header">
                     <h5 class="card-title">{{__('Graficas de Consumos')}}</h5>
                     <div class="card-tools">
-                        <?php if ($pItem="" && $pReference=""){ echo '<a  href="Ereport/'.$pID.'/'.$pTipo.'/'.$pDepartamento.'/'.$pItem.'/'.$pReference.'/'.$pFrom.'/'.$pTo.'" type="button" class="btn btn-tool">
+                        <?php if ($pItem=="" && $pReference==""){ echo '<a  href="Ereport/'.$pID.'/'.$pTipo.'/'.$pDepartamento.'/'.$pItem.'/'.$pReference.'/'.$pFrom.'/'.$pTo.'" type="button" class="btn btn-tool">
                             '.__('Descargar reporte').'
                             <i class="fas fa-file-excel"></i>
-                        </a>'; }elseif($pItem=""){
+                        </a>'; }elseif($pItem==""){
                             echo '<a  href="EreportI/'.$pID.'/'.$pTipo.'/'.$pDepartamento.'/'.$pItem.'/'.$pFrom.'/'.$pTo.'" type="button" class="btn btn-tool">
                             '.__('Descargar reporte').'
                             <i class="fas fa-file-excel"></i>
-                        </a>';} elseif ($pReference="") {
+                        </a>';} elseif ($pReference=="") {
                             echo '<a  href="EreportR/'.$pID.'/'.$pTipo.'/'.$pDepartamento.'/'.$pReference.'/'.$pFrom.'/'.$pTo.'" type="button" class="btn btn-tool">
                             '.__('Descargar reporte').'
                             <i class="fas fa-file-excel"></i>
