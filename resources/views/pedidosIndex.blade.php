295aea331c7c0a963f0d4ea74965660fba2344e6
Carlos Enrique Guizar Peña <71019682+Skullnietz@users.noreply.github.com>
2023-04-14 11:13:34 -0600

    Pedidos

--- resources/views/pedidosIndex.blade.php
+++ resources/views/pedidosIndex.blade.php
@@ -27,12 +27,12 @@
         <div class="col-md-12 col-sm-12 col-xs-12">
         <div class="card">
             <div class="card-header">
-                {{$pedido->Pedido}} | {{$pedido->Fecha}}
+                {{$pedido->Fecha}}
             </div>
             <div class="card-body">
                     <div class="row">
-                    <div class="col-3">Fotos Pedido</div>
-                    <div class="col-6">
+                    <div class="col-3">{{$pedido->Pedido}} </div>
+                    <div class="col-4">
                         @foreach ($pedido->desc as $descpedido)
                         <div class="row">
                             {{$descpedido->art->Descripcion1}}
