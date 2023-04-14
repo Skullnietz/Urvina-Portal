584ef3f532ef020567d4dfe2e84c64cce4b1f29e
Carlos Enrique Guizar Peña <71019682+Skullnietz@users.noreply.github.com>
2023-04-14 09:21:01 -0600

    Pedidos

--- resources/views/pedidosIndex.blade.php
+++ resources/views/pedidosIndex.blade.php
@@ -22,7 +22,7 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-3 col-sm-6 col-xs-6">
+        <div class="col-md-12 col-sm-12 col-xs-12">
         <div class="card">
             <div class="card-header">
                 Fecha
