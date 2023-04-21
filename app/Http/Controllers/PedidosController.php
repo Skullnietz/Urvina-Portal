dc178ff71ee4540e6c13c0a5e1d3aa69d5159d9d
Carlos Enrique Guizar Peña <71019682+Skullnietz@users.noreply.github.com>
2023-04-21 13:27:29 -0600

    Pedidos

--- app/Http/Controllers/PedidosController.php
+++ app/Http/Controllers/PedidosController.php
@@ -54,7 +54,7 @@ class PedidosController extends Controller
         //////////////////////////////////////////////////
 
     }
-    public function show($lang,$id){
+    public function show($id){
         session_start();
         //////////////////// Vista Pedidos /////////////////
         $data = array();
