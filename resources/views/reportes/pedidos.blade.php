f6d494982ff377e58a4db7ed70a5be7618ee98ac
Carlos Enrique Guizar Peña <71019682+Skullnietz@users.noreply.github.com>
2023-06-09 13:42:18 -0600

    Ajustes Generales (Junta)

--- resources/views/reportes/pedidos.blade.php
+++ resources/views/reportes/pedidos.blade.php
@@ -421,7 +421,7 @@ display: none !important;
                                     <td>{{$pedido->Pedido}}</td>
                                     <td><ul>@foreach ($pedido->desc as $item)
 
-                                            <li>{{__($item->Descripcion)}}</li>
+                                            <li>{{__($item->Descripcion)}}({{($item->Cantidad)}})</li>
 
 
                                         @endforeach</ul>
@@ -458,7 +458,7 @@ display: none !important;
                                     <td>{{$pedido->Pedido}}</td>
                                     <td><ul>@foreach ($pedido->desc as $item)
 
-                                            <li>{{__($item->Descripcion)}}</li>
+                                            <li>{{__($item->Descripcion)}}({{($item->Cantidad)}})</li>
 
 
                                         @endforeach</ul>
